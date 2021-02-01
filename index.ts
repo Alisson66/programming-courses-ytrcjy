e89d6cec3b11a47eb2c2199b43f7c33edd887078
Alisson66 <alis2019ss@gmail.com>
2021-02-01 10:01:25 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -2,3 +2,13 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
+let ranck1: number;
+let ranck2: number;
+let ranck3: number;
+let ranck4: number;
+ranck1 = 12;
+ranck2 = 89;
+ranck3 = 34;
+ranck4 = 51;
+
+console.log(ranck2 - ranck1 + ranck4 * ranck3);
